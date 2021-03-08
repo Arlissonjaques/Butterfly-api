27b1a12fbd5b3464cd1e7c7ce589481937deadbc
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-07 21:07:06 -0300

    feat(product_controller): added controller for products

---
+++ app/controllers/api/products_controller.rb
@@ -0,0 +1,40 @@
+class Api::ProductsController < ApplicationController
+
+  before_action :set_product, only: [:update, :destroy]
+
+  def index
+    render json: Product.all
+  end
+  #------------------------------
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      render json: @product, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+  #------------------------------
+  def update
+    if @product.update(product_params)
+      render json: @product, status: :ok
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+  #------------------------------
+  def destroy
+    @product.destroy
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.permit(:name, :color, :description, :category_id)
+  end
+end
