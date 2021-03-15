745d5c7205df5f4f47b8f08e6557fd0f3bf49db6
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-14 21:48:33 -0300

    feat(category_controller): added controller for category

---
+++ app/controllers/api/category_controller.rb
@@ -0,0 +1,41 @@
+class Api::CategoryController < ApplicationController
+
+  before_action :set_category, only: [:update, :destroy]
+
+  def index
+    render json: Category.all
+  end
+  #------------------------------
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      render json: @category, status: :created
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+  #------------------------------
+  def update
+    if @category.update(category_params)
+      render json: @category, status: :ok
+    else
+      render json: @category.errors, status: :unprocessable_entity
+    end
+  end
+  #------------------------------
+  def destroy
+    @category.destroy
+  end
+
+  private
+
+  def set_category
+    @category = Category.find(params[:id])
+  end
+
+  def category_params
+    params.permit(:name)
+  end
+  
+end
