b3a45c26ec8661ec9e8418a1f5399b67f46954f5
Arlisson Jaques <arlissonjaques@gmail.com>
2021-03-16 11:28:21 -0300

    feat(contact_controller): added controller for contacts

---
+++ app/controllers/api/contacts_controller.rb
@@ -0,0 +1,43 @@
+class Api::ContactsController < ApplicationController
+
+  before_action :set_contact, only: [:update, :destroy]
+
+  def index
+    render json: Contact.all
+  end
+  #-----------------------------------------------------------------------
+  def show
+  end
+  #-----------------------------------------------------------------------
+  def create
+    @contact = Contact.new(contact_params)
+
+    if @contact.save
+      render json: @contact, status: :created
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+  #-----------------------------------------------------------------------
+  def update
+    if @contact.update(contact_params)
+      render json: @contact, status: :ok
+    else
+      render json: @contact.errors, status: :unprocessable_entity
+    end
+  end
+  #-----------------------------------------------------------------------
+  def destroy
+    @contact.destroy
+  end
+
+  private
+
+  def set_contact
+    @contact = Contact.find(params[:id])
+  end
+  #-----------------------------------------------------------------------
+  def contact_params
+    params.permit(:firstname, :lastname, :message, :email, :product_id)
+  end
+end
