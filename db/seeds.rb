cd92a8afe99c77a02f808b1f9082b9b4961de3db
Arlisson Jaques <arlissonjaques@gmail.com>
2021-05-02 20:36:39 -0300

    feat(seed): create user default

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create({
+              "firstname": 'Arlisson',
+              "lastname": 'Jaques',
+              "email": 'arlisson@jaques.com',
+              "password": 'arlisson123'
+            })
