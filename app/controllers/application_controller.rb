d5a66b8b74cec0af8e9a67e21bc618a26bc21c71
Arlisson Jaques <arlissonjaques@gmail.com>
2021-05-02 20:33:14 -0300

    feat(application_controller): function added to allow other parameters to pass

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::API
-   include DeviseTokenAuth::Concerns::SetUserByToken
+  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname lastname email])
+  end
 end
