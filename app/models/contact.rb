f6624441538f03a3bda31b9284c8742f53ddbcda
Arlisson Jaques <arlissonjaques@gmail.com>
2021-06-23 11:02:09 -0300

    refactor(contact): changing validation

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -14,7 +14,6 @@
 class Contact < ApplicationRecord
   belongs_to :product
 
-  validates :firstname, :lastname, :message, presence: true
-  validates :email, uniqueness: true
+  validates :firstname, :lastname, :email, :message, presence: true
   validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/
 end
